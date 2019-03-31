74c50220d5f529f2251576dbf1826e42f8a564c5
Judy <mokjooyeon00@gmail.com>
2019-03-31 02:17:38 -0400

    Second commit

--- prgFriendsBook/WriteMessage.aspx.cs
+++ prgFriendsBook/WriteMessage.aspx.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
 
 public partial class WriteMessage : System.Web.UI.Page
 {
-
+    static OleDbConnection myCon;
+    static OleDbDataReader myRdr;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        myCon = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("~/App_Data/friendsbookDB1.mdb"));
+        myCon.Open();
+        OleDbCommand myCmd = new OleDbCommand("select UserID, FirstName, LastName, UserName from Users", myCon);
+        myRdr = myCmd.ExecuteReader();
+    }
+    private void fillCboBox()
+    {
+        while(myRdr.Read())
+        {
+            ListItem temp = new ListItem();
+            temp.Text = myRdr["FirstName"].ToString() + ", " + myRdr["LastName"].ToString() + " - " + myRdr["UserName"].ToString();
+            temp.Value = myRdr["UserID"].ToString();
+        }
     }
 }
